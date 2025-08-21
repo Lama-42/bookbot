48451b05a6864488eff0d9baf7fdab7f9048f1ff
Lama <ralfschlemmer@hotmail.de>
2025-08-21 19:55:26 +0200

    D:

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ def get_book_text(path_to_file):
     return file_contents
 
 from stats import word_count
+from stats import num_characters
+
 
 def main():
     book = get_book_text("books/frankenstein.txt")
     #print(book) 
     print(f"{word_count(book)} words found in the document")
+    print(f"{num_characters(book)}")
 
 main()
-
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def word_count(book):
     words = book.split()
     return len(words)
+
+def num_characters(book):
+    num_chars = {}
+    no_dupes = book.lower()
+    
+    for char in no_dupes:
+        if char not in num_chars:
+            num_chars[char] = 1
+        else:
+            num_chars[char] += 1
+    return num_chars
