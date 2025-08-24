10861207d23f4513fa5a8e6de230ef53327d98cf
Lama <ralfschlemmer@hotmail.de>
2025-08-24 18:25:37 +0200

    F:

--- main.py
+++ main.py
@@ -5,12 +5,35 @@ def get_book_text(path_to_file):
 
 from stats import word_count
 from stats import num_characters
+from stats import convert_sort
+
 
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
-    #print(book) 
-    print(f"{word_count(book)} words found in the document")
-    print(f"{num_characters(book)}")
-
+    book_path = ("books/frankenstein.txt")
+    book = get_book_text(book_path)
+    stats = num_characters(book)
+    sorted_stats = convert_sort(stats)
+    
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count(book)} total words")
+    print("--------- Character Count -------")
+    for item in sorted_stats:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
 main()
+
+
+
+
+
+
+
+
+    #print(f"{word_count(book)} words found in the document")
+    #print(f"{num_characters(book)}")
+    #print(f"{convert_sort(stats)}")
+
